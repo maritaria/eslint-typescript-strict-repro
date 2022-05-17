f8bd52f15dc006d649b2ccc6e105376acc318622
bramkamies <bramkamies@passionatepeople.io>
2022-05-17 17:08:01 +0200

    Finish typescript setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['plugin:@typescript-eslint/strict'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json']
